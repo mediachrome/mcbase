0b1d3bdb3346a3dfb884df85666998881955378b
James Bisset <jbisset@mediachrome.com>
2012-05-18 16:16:54 +0100

    updates to the show/hide menu facility for small screen devices

--- js/responsive.js
+++ js/responsive.js
@@ -3,18 +3,25 @@
   Drupal.behaviors.mcbase_responsive = {
     
     attach: function(context, settings) {
-  
    
-      // detect window size from css
+      // detect window size from value set by CSS
+      // TODO This also needs to reun on resize action
       var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
-      
+/*
+      $(window).resize(function(){
+        var newsize = window.getComputedStyle(document.body,':after').getPropertyValue('content');
+        alert('newsize = ' + newsize);
+      });
+
+      if(newsize) { size = newsize; } 
+    
+*/
       // alert('size = ' + size);
-      
-      // if (size == 'phone') {
+      if (size == 'phone') {
 
         // add an element-invisible class to the menu 
-        // $('#main-menu').addClass('element-invisible');
-        $('#navigation').addClass('padding');
+        $('#main-menu').addClass('hidden-menu');
+        $('#navigation').addClass('phone-navigation');
         
         // add menu button id to the nav title
         $('#navigation h2').removeClass('element-invisible');
@@ -23,9 +30,9 @@
         // set a toggle action on the nav title to hide and show the menu */
         
         $('#menu-toggle').bind('click', function(){
-          $("#main-menu").toggleClass('element-invisible');
+          $("#main-menu").toggleClass('hidden-menu');
         });
-      // }
+      }
 
 
     }
