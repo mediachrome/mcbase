74074ed8d9e89847db6c3ee9008c6c9568af329d
James Bisset <jbisset@mediachrome.com>
2012-07-17 10:55:35 +0100

    fixed bug which removed limiter div

--- js/responsive.js
+++ js/responsive.js
@@ -5,36 +5,90 @@
     attach: function(context, settings) {
    
       // detect window size from value set by CSS
-      // TODO This also needs to reun on resize action
-      var size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
-/*
-      $(window).resize(function(){
-        var newsize = window.getComputedStyle(document.body,':after').getPropertyValue('content');
-        alert('newsize = ' + newsize);
+      // with thanks to Matt Wilcox for the window.resize stuff
+      // http://mattwilcox.net/archive/entry/id/1088/
+      
+      var current_size; // defaults to blank so it's always analysed on first load
+      var did_resize  = true; // defaults to true so it's always analysed on first load
+      var navigation_classes = $('#navigation').attr("class"); // store the default #navigation classes
+      var raw_navigation = $("#navigation").html(); // grab the unaltered #navigation contents and store it for simpler resets on resize
+     
+      // on window resize, set the didResize to true
+      $(window).resize(function() {
+        did_resize = true;
       });
+      
+      // every 1/4 second, check if the browser was resized
+      setInterval(function() {
+        if(did_resize) {
+          did_resize = false;
 
-      if(newsize) { size = newsize; } 
-    
+          var new_size = window.getComputedStyle(document.body,':after').getPropertyValue('content');
+          /* tidy up after inconsistent browsers (some include quotation marks, they shouldn't) */
+          new_size = new_size.replace(/"/g, "");
+          new_size = new_size.replace(/'/g, "");
+          
+          // sweep clean before we make any changes
+          // if the new breakpoint is different to the old one, do some stuff
+          
+          if (new_size != current_size) {
+            
+            // print this out somewhere we can see
+            /*
+$("#responsive-indicator .current-size").remove();
+            $("#responsive-indicator").append("<div class='current-size'></div>");
+            $(".current-size").html(current_size + ' / ' + new_size);
 */
-      // alert('size = ' + size);
-      if (size == 'phone') {
-
-        // add an element-invisible class to the menu 
-        $('#main-menu').addClass('hidden-menu');
-        $('#navigation').addClass('phone-navigation');
         
-        // add menu button id to the nav title
-        $('#navigation h2').removeClass('element-invisible');
-        $('#navigation h2').attr('id', 'menu-toggle');
-        
-        // set a toggle action on the nav title to hide and show the menu */
-        
-        $('#menu-toggle').bind('click', function(){
-          $("#main-menu").toggleClass('hidden-menu');
-        });
-      }
+            // remove the old #navigation content)
+            $("#navigation .limiter").remove();
 
+            // now re-insert clean mark-up
+            $("#navigation").html(raw_navigation);
 
+            // alert('size = ' + size);
+            
+            if (new_size === 'widescreen') {
+            
+            
+            // set the current size
+              current_size = new_size;
+            }
+            
+            if (new_size === 'standard') {
+            
+            
+            // set the current size
+              current_size = new_size;
+            }
+
+            if (new_size === 'tablet') {
+            
+            
+            // set the current size
+              current_size = new_size;
+            }
+
+            
+            
+            if (new_size === 'phone') {
+  
+              // add an element-invisible class to the menu 
+              $('#navigation').addClass('phone-navigation');
+              $('#main-menu').addClass('hidden-menu');
+              
+              // add menu button id to the nav title
+              $('#navigation h2').removeClass('element-invisible');
+              $('#navigation h2').attr('id', 'menu-toggle');
+              
+              // set the current size
+              current_size = new_size;
+            }
+            
+            
+          }
+        }
+      }, 250);
     }
   };
 }(jQuery));
